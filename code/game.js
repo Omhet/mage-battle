c7f116070ca841588fe87af76b6c20de2772e23c
Omhet <omhetehmo@yandex.ru>
2019-03-22 10:48:33 +0300

    Switch to Phaser

--- code/game.js
+++ code/game.js
@@ -1,196 +1,157 @@
-const g = ga(512, 512, setup, [
-  '../images/fantasy.png',
-  '../images/walkcycle.png',
-  '../maps/fantasy.json'
-]);
-g.start();
-
-g.fps = 60;
-
-let world,
-  elf,
-  elfTextures,
-  camera,
-  itemsLayer,
-  itemsMapArray,
-  items,
-  message,
-  bullets;
-
-function setup() {
-  world = g.makeTiledWorld('../maps/fantasy.json', '../images/fantasy.png');
-
-  elf = g.sprite(g.filmstrip('../images/walkcycle.png', 64, 64));
-
-  elf.x = world.getObject('elf').x;
-  elf.y = world.getObject('elf').y;
-
-  const objectsLayer = world.getObject('objects');
-  objectsLayer.addChild(elf);
-
-  itemsLayer = world.getObject('items');
-  items = itemsLayer.children.slice(0);
-  itemsMapArray = world.getObject('items').data;
-
-  camera = g.worldCamera(world, g.canvas);
-  camera.centerOver(elf);
-
-  elf.collisionArea = { x: 22, y: 44, width: 20, height: 20 };
-
-  elf.states = {
-    up: 0,
-    left: 9,
-    down: 18,
-    right: 27,
-    walkUp: [1, 8],
-    walkLeft: [10, 17],
-    walkDown: [19, 26],
-    walkRight: [28, 35]
-  };
-
-  elf.show(elf.states.right);
-  elf.fps = 18;
-
-  setKeyboard();
-
-  ex = elf.gx + elf.halfWidth;
-  ey = elf.gy;
-
-  rect = g.rectangle(10, 10, 'none', 'none', 0, ex, ey);
-  // rect = g.rectangle(10, 10, 'none', 'black', 1, elf.centerX - camera.leftInnerBoundary, camera.centerY - elf.height);
-
-  message = g.text('No items found', '12px Verdana', 'black');
-  message.setPosition(10, 10);
-  message.visible = false;
-
-  bullets = [];
-
-  g.stage.interactive = true;
-  g.stage.press = function() {
-    g.shoot(
-      rect, //The shooter
-      rect.rotation, //The angle at which to shoot
-      0, //The bullet's offset from the center
-      7, //The bullet's speed (pixels per frame)
-      bullets, //The array used to store the bullets
-      //A function that returns the sprite that should
-      //be used to make each bullet
-      function() {
-        return g.circle(8, 'red');
+const scene = new Phaser.Scene();
+const config = {
+  type: Phaser.AUTO,
+  width: 768,
+  height: 512,
+  parent: 'game',
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        y: 0
       }
-    );
-  };
-
-  g.state = play;
-}
-
-function play() {
-  elf.x = Math.max(-18, Math.min(elf.x + elf.vx, world.width - elf.width + 18));
-  elf.y = Math.max(-10, Math.min(elf.y + elf.vy, world.height - elf.height));
-  //   g.move(elf);
-  //   if (elf.x > world.width) elf.x = 0;
-  //   if (elf.x < 0) elf.x = world.width;
-  camera.follow(elf);
-  elfVsGround();
-  elfVsItems();
-
-  rect.x = elf.gx + elf.halfWidth - 2;
-  rect.y = elf.gy + elf.halfHeight;
-  rect.rotation = g.angle(rect, g.pointer)
-
-
-  bullets = bullets.filter(function(bullet) {
-    g.move(bullet);
-    var collision = g.outsideBounds(bullet, g.stage.localBounds);
-    if (collision) {
-      g.remove(bullet);
-      return false;
     }
-    return true;
+  },
+  scene
+};
+
+const game = new Phaser.Game(config);
+let controls;
+
+scene.preload = function() {
+  // World
+  this.load.image('tiles', 'maps/world-1/world.png');
+  this.load.tilemapTiledJSON('map', 'maps/world-1/world.json');
+
+  // Hero
+  this.load.atlas('atlas', 'heroes/misa/atlas.png', 'heroes/misa/atlas.json');
+};
+
+scene.create = function() {
+  const map = this.make.tilemap({ key: 'map' });
+
+  const tileset = map.addTilesetImage('world', 'tiles');
+
+  const groundLayer = map.createStaticLayer('ground', tileset, 0, 0);
+  const worldLayer = map.createStaticLayer('world', tileset, 0, 0);
+
+  worldLayer.setDepth(10);
+  worldLayer.setCollisionByProperty({ collides: true });
+
+  player = this.physics.add
+    .sprite(64, 64, 'atlas', 'misa-front')
+    .setSize(30, 40)
+  .setOffset(0, 24);
+  this.physics.add.collider(player, worldLayer);
+
+  const anims = this.anims;
+  anims.create({
+    key: 'misa-left-walk',
+    frames: anims.generateFrameNames('atlas', {
+      prefix: 'misa-left-walk.',
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
+    frameRate: 10,
+    repeat: -1
   });
-}
+  anims.create({
+    key: 'misa-right-walk',
+    frames: anims.generateFrameNames('atlas', {
+      prefix: 'misa-right-walk.',
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: 'misa-front-walk',
+    frames: anims.generateFrameNames('atlas', {
+      prefix: 'misa-front-walk.',
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: 'misa-back-walk',
+    frames: anims.generateFrameNames('atlas', {
+      prefix: 'misa-back-walk.',
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
 
-function elfVsGround() {
-  const obstaclesMapArray = world.getObject('obstacles').data;
-  const elfVsGround = g.hitTestTile(elf, obstaclesMapArray, 0, world, 'every');
-  if (!elfVsGround.hit) {
-    elf.x -= elf.vx;
-    elf.y -= elf.vy;
-    elf.vx = 0;
-    elf.vy = 0;
+  const camera = this.cameras.main;
+
+  cursors = this.input.keyboard.createCursorKeys();
+  // const { left, right, up, down } = cursors;
+  // controls = new Phaser.Cameras.Controls.FixedKeyControl({
+  //   camera,
+  //   left,
+  //   right,
+  //   up,
+  //   down,
+  //   speed: 0.5
+  // });
+
+  camera.startFollow(player);
+  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+};
+
+const speed = 175;
+scene.update = function(time, delta) {
+  // controls.update(delta);
+
+  const prevVelocity = player.body.velocity.clone();
+
+  // Stop any previous movement from the last frame
+  player.body.setVelocity(0);
+
+  // Horizontal movement
+  if (cursors.left.isDown) {
+    player.body.setVelocityX(-speed);
+  } else if (cursors.right.isDown) {
+    player.body.setVelocityX(speed);
   }
-}
 
-function elfVsItems() {
-  const elfVsItems = g.hitTestTile(elf, itemsMapArray, 0, world, 'some');
-  if (!elfVsItems.hit) {
-    items = items.filter(function(item) {
-      if (item.index === elfVsItems.index) {
-        message.visible = true;
-        message.content = 'You found a ' + item.name;
-        g.wait(3000, function() {
-          message.visible = false;
-        });
-        itemsMapArray[item.index] = 0;
-        g.remove(item);
-        return false;
-      } else {
-        return true;
-      }
-    });
+  // Vertical movement
+  if (cursors.up.isDown) {
+    player.body.setVelocityY(-speed);
+  } else if (cursors.down.isDown) {
+    player.body.setVelocityY(speed);
   }
-}
 
-function setKeyboard() {
-  leftArrow = g.keyboard(37);
-  upArrow = g.keyboard(38);
-  rightArrow = g.keyboard(39);
-  downArrow = g.keyboard(40);
+  // Normalize and scale the velocity so that player can't move faster along a diagonal
+  player.body.velocity.normalize().scale(speed);
 
-  leftArrow.press = function() {
-    // rect.rotation = 3,14159;
-    elf.playSequence(elf.states.walkLeft);
-    elf.vx = -2;
-    elf.vy = 0;
-  };
-  leftArrow.release = function() {
-    if (!rightArrow.isDown && elf.vy === 0) {
-      elf.vx = 0;
-      elf.show(elf.states.left);
-    }
-  };
-  upArrow.press = function() {
-    elf.playSequence(elf.states.walkUp);
-    elf.vy = -2;
-    elf.vx = 0;
-  };
-  upArrow.release = function() {
-    if (!downArrow.isDown && elf.vx === 0) {
-      elf.vy = 0;
-      elf.show(elf.states.up);
-    }
-  };
-  rightArrow.press = function() {
-    // rect.rotation = 0;
-    elf.playSequence(elf.states.walkRight);
-    elf.vx = 2;
-    elf.vy = 0;
-  };
-  rightArrow.release = function() {
-    if (!leftArrow.isDown && elf.vy === 0) {
-      elf.vx = 0;
-      elf.show(elf.states.right);
-    }
-  };
-  downArrow.press = function() {
-    elf.playSequence(elf.states.walkDown);
-    elf.vy = 2;
-    elf.vx = 0;
-  };
-  downArrow.release = function() {
-    if (!upArrow.isDown && elf.vx === 0) {
-      elf.vy = 0;
-      elf.show(elf.states.down);
-    }
-  };
-}
+  
+  // Update the animation last and give left/right animations precedence over up/down animations
+  if (cursors.left.isDown) {
+    player.anims.play("misa-left-walk", true);
+  } else if (cursors.right.isDown) {
+    player.anims.play("misa-right-walk", true);
+  } else if (cursors.up.isDown) {
+    player.anims.play("misa-back-walk", true);
+  } else if (cursors.down.isDown) {
+    player.anims.play("misa-front-walk", true);
+  } else {
+    player.anims.stop();
+  }
+
+  // If we were moving, pick and idle frame to use
+  // if (prevVelocity.x < 0) player.setTexture("atlas", "misa-left");
+  // else if (prevVelocity.x > 0) player.setTexture("atlas", "misa-right");
+  // else if (prevVelocity.y < 0) player.setTexture("atlas", "misa-back");
+  // else if (prevVelocity.y > 0) player.setTexture("atlas", "misa-front");
+
+};
